be834d42fb3be888fcdd8a21531de406cc258f2f
AdiChakote <adichakote@gmail.com>
2025-08-05 14:20:04 +0530

    Fix import: use correct casing for Mygigs.css

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./MyGigs.css";
+import "./Mygigs.css";
 import getCurrentUser from "../../utils/getCurrentUser";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
